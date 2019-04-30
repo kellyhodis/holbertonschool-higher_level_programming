a2fa2c1995c9301e21b7aa681e1f4c1c8e5ea269
Kelly Hodis <668@holbertonschool.com>
2019-04-30 00:50:02 +0000

    First commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks if a singly linked list has a cycle in it
+* @list: singly linked list to check
+*
+* Return: 0 if no cycle or 1 if cycle
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	slow = list;
+	fast = list;
+
+	while (slow && fast)
+	{
+		slow = slow->next;
+		if (fast->next)
+			fast = fast->next->next;
+		else
+			fast = fast->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
