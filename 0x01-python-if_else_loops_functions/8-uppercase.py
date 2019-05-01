f374af3088bcc81108b287e4e81cfcf46c18d228
Kelly Hodis <668@holbertonschool.com>
2019-05-01 01:55:28 +0000

    Restructured program and condensed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    length = len(str)
     for i in str:
-        length = length - 1
-        if length == 0:
-            spaces = "\n"
-        else:
-            spaces = ""
         if ord(i) > 96 and ord(i) < 123:
-            print("{:c}".format(ord(i) - 32), end=spaces)
-        else:
-            print("{}".format(i), end=spaces)
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+        print()
