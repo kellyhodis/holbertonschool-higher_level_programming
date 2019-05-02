be4e9e35ff2def8af43090713749159c4ba2aab2
Kelly Hodis <668@holbertonschool.com>
2019-05-02 21:27:39 +0000

    Changed spacing and word formatting

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,14 @@
 from sys import argv
 if __name__ == '__main__':
     if len(argv) == 1:
-        char = ". "
-    else:
-        char = ": "
+        char = "argument."
+    elif len(argv) == 2:
+        char = "argument:"
         number = 1
-    print("{:d} arguments{}".format(len(argv) - 1, char))
+    else:
+        char = "arguments:"
+        number = 1
+    print("{:d} {}".format(len(argv) - 1, char))
     for i in argv[1:]:
         print("{:d}: {}".format(number, i))
         number = number + 1
