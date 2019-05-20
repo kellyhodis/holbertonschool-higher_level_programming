dd21f54efd0ff103e30c53f5890887dc3e1f71f2
Kelly Hodis <668@holbertonschool.com>
2019-05-20 19:14:22 +0000

    Fixed loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     num = 0
     try:
-        for i in my_list:
-            print("{}".format(i), end="")
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
             num = num + 1
             if num == x:
                 break
