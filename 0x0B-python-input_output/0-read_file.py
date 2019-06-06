9c0ebec3e5e7eda062d8a072e521dc0b3478207d
Kelly Hodis <668@holbertonschool.com>
2019-06-06 21:37:31 +0000

    Fixed function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,7 +7,7 @@ module to read a file
 def read_file(filename=""):
     """function to read file
     """
-    f = open("my_file_0.txt", "r")
+    f = open(filename, "r")
     if f.mode == 'r':
         contents = f.read()
     print(contents, end="")
