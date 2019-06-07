1f683c26311203a4497fd92fc1075f34345a2f5a
Kelly Hodis <668@holbertonschool.com>
2019-06-07 02:22:24 +0000

    First commit

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""module to convert to JSON
+"""
+
+
+def to_json_string(my_obj):
+    """function that returns JSON representation of object (string)
+    """
+    return json.dumps(my_obj)
