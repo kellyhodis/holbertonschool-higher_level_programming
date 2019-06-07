c0d687d5408d8de1c2a2745b46ba31de987236c1
Kelly Hodis <668@holbertonschool.com>
2019-06-07 20:07:41 +0000

    First commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module that has lookup function
+"""
+
+
+def lookup(obj):
+    """function that returns the list of
+    available attributes and methods of an object
+    """
+    return dir(obj)
