0ef96c2c8f9a8007dc38f3032752fbb042e73435
Kelly Hodis <668@holbertonschool.com>
2019-06-07 03:27:05 +0000

    Included module

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 """module to load, add, and save
 """
 
 
 py_list = []
+py_list = load_from_json_file("add_item.json")
 if len(sys.argv) > 1:
     for i in range (1, len(sys.argv)):
         py_list.append(sys.argv[i])
