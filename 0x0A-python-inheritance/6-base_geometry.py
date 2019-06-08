dc838f5f4ca60d1aa32851c09a7ff81de85135c9
Kelly Hodis <668@holbertonschool.com>
2019-06-08 06:17:58 +0000

    First commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""geometry module
+"""
+
+
+class BaseGeometry:
+    """class containing area method
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
