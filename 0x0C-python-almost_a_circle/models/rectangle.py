e95d7aba39db82b9be2da237d50904cb579f95bb
Kelly Hodis <668@holbertonschool.com>
2019-06-10 09:45:14 +0000

    First commit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module for Rectangle class
+"""
+
+
+class Rectangle(Base):
+    """class that inherits from Base
+    """
+    def __init__(self, width, height, x=0, y=0, id=None)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super(id, self).__init__(id)
+
+    def 
