536447623c44ece7bde0c5961a56b94c62a9192c
Kelly Hodis <668@holbertonschool.com>
2019-06-12 21:11:23 +0000

    Documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,20 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """return JSON representation of object
+        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """write JSON to file
+        """
+        filename = "{}.json".format(list_objs[0].__class__.__name__)
+        with open(filename, "w+") as file:
+            else:
+                for i in list_objs:
+                    file.write(cls.to_json_string(i), default="[]")
+
