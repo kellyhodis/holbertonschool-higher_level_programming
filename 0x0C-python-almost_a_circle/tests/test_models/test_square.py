9a5acc9ae5d554c76d71a923201575447d6353ca
Kelly Hodis <668@holbertonschool.com>
2019-06-13 04:30:03 +0000

    First commit

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Unittest for Square class
+"""
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """unittest class
+    """
+    def test___init(self):
+        """test for __init__ method
+        """
+
+    def test___str(self):
+        """test for __str__ method
+        """
+
+    def test_update(self):
+        """test for update method
+        """
+
+    def test_to_dictionary(self):
+        """test for to_dictionary method
+        """
+
+    def test_size_getter(self):
+        """test for size getter
+        """
+
+    def test_size_setter(self):
+        """test for size setter
+        """
