5d92d2b8ab9943e2252daee1edb40cef87d7a483
Kelly <kelly.hodis@protonmail.com>
2019-06-13 21:23:37 +0000

    Added Square initialization tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -18,6 +18,41 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(r.x, 0)
         self.assertEqual(r.y, 0)
 
+        # check that size must be >= 0
+        with self.assertRaises(ValueError):
+            v = Square(-1)
+
+        with self.assertRaises(ValueError):
+            u = Square(0)
+
+        # check that size must be an integer
+        with self.assertRaises(TypeError):
+            w = Square("1")
+
+        # check that x must be an integer
+        with self.assertRaises(TypeError):
+            z = Square(1, "1")
+
+        # check that x must be >= 0
+        with self.assertRaises(ValueError):
+            c = Square(1, -1)
+
+        # check that y must be >= 0
+        with self.assertRaises(ValueError):
+            d = Square(1, 1, -1)
+
+        # check that y must be an integer
+        with self.assertRaises(TypeError):
+            a = Square(1, 1, "1")
+
+        # check that assignments are correct
+        b = Square(1, 1, 1, 1)
+        self.assertEqual(b.width, 1)
+        self.assertEqual(b.height, 1)
+        self.assertEqual(b.x, 1)
+        self.assertEqual(b.y, 1)
+        self.assertEqual(b.id, 1)
+
     def test___str(self):
         """test for __str__ method
         """
@@ -65,6 +100,26 @@ class TestSquare(unittest.TestCase):
         with self.assertRaises(TypeError):
             r.size = "r"
 
+    def test_square_save_to_file(self):
+        """test for save_to_file with Square
+        """
+        s = Square(3)
+
+        l = []
+        l.append(s)
+        s.save_to_file(l)
+
+        # test that file is not empty
+        with open('Square.json') as file:
+            self.assertTrue(file.read(1))
+
+        # test saving empty list to file
+        p = []
+        s.save_to_file(p)
+
+        with open('Square.json') as file:
+            self.assertTrue(file.read(1))
+
     def test_square_load_from_file(self):
         """test for load_from_file with Square
         """
@@ -72,4 +127,13 @@ class TestSquare(unittest.TestCase):
 
         # check when file doesn't exist
         l = s.load_from_file()
+
         self.assertIsInstance(l, list)
+
+        # check when file does exist
+        li = []
+        li.append(s)
+        s.save_to_file(li)
+        l = s.load_from_file()
+        self.assertIsInstance(l, list)
+
