4161ed62b88be7356bb62af7a22b2cd972009ca2
Kelly Hodis <668@holbertonschool.com>
2019-08-10 15:15:43 -0400

    Create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This is a module containing a script that lists
+all states from the database hbtn_0e_0_usa."""
+
+import sys
+import MySQLdb
+
+
+def list_all(mysql_username="", mysql_password="", database_name=""):
+    ''' This is a function that lists all states from a database. '''
+    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                           passwd=mysql_password, db=database_name,
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    list_all(username, password, database)
