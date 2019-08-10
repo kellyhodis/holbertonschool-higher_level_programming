46d333f53483f66b7a1849c8a2717e9bf030dda8
Kelly Hodis <668@holbertonschool.com>
2019-08-10 16:12:52 -0400

    Edit syntax

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,18 +5,18 @@ from the database hbtn_0e_0_usa."""
 import MySQLdb
 import sys
 
-
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                       passwd=password, db=database,
-                       charset="utf8")
-cur = conn.cursor()
-sql = "SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC"
-cur.execute(sql)
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database,
+                           charset="utf8")
+    cur = conn.cursor()
+    sql = "SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC"
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
