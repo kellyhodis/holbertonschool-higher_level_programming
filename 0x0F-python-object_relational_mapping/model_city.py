5ea8a68e12b53021f5ed3d4a57edca9f9de09063
Kelly Hodis <668@holbertonschool.com>
2019-08-10 19:28:00 -0400

    Create model_city.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This is a module to define the City class. """
+
+from sqlalchemy import Column, Integer, String
+from model_state import Base, State
+from sqlalchemy import ForeignKey
+
+
+class City(Base):
+    ''' Definition of City class, inheriting from Base
+    in Base of model_state. '''
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'),
+                      nullable=False)
