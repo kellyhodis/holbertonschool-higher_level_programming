3de5adcdd89c834f613950c3c7b0356680931fa4
Kelly Hodis <668@holbertonschool.com>
2019-08-10 16:10:38 -0400

    Remove function

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,23 +6,17 @@ import MySQLdb
 import sys
 
 
-def list_all_n(mysql_username="", mysql_password="", database_name=""):
-    ''' This is a script that lists all states with a name starting with N.'''
-    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                           passwd=mysql_password, db=database_name,
-                           charset="utf8")
-    cur = conn.cursor()
-    sql = "SELECT * FROM states WHERE name REGEXP '^N' ORDER BY states.id ASC"
-    cur.execute(sql)
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
-
-
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    list_all_n(username, password, database)
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                       passwd=password, db=database,
+                       charset="utf8")
+cur = conn.cursor()
+sql = "SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC"
+cur.execute(sql)
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
