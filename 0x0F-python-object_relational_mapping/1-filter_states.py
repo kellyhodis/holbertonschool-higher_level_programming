05f89c1e9cbd0cb54d32ad38100380c50fb93543
Kelly Hodis <668@holbertonschool.com>
2019-08-10 15:22:20 -0400

    Edit SQL

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,13 +6,13 @@ import MySQLdb
 import sys
 
 
-def list_all_n(mysql_username, mysql_password, database_name):
+def list_all_n(mysql_username="", mysql_password="", database_name=""):
     ''' This is a script that lists all states with a name starting with N.'''
     conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
                            passwd=mysql_password, db=database_name,
                            charset="utf8")
     cur = conn.cursor()
-    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    sql = "SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC"
     cur.execute(sql)
     query_rows = cur.fetchall()
     for row in query_rows:
