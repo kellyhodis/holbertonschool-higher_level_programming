e0368ab9581b04b24fd9ac600d0dbac86b4f38cc
kellyhodis <668@holbertonschool.com>
2019-08-13 00:58:18 -0400

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Create the database hbtn_0d_2 and the user user_0d_2.
 CREATE IF NOT EXISTS USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE IF NOT EXISTS hbtn_0d_2;
-GRANT USAGE ON *.* TO 'user_0d_1'@'localhost'
+GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
