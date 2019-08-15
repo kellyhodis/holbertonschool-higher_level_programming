5aed26bae326b3b721ada7e13b6266db785fdf6b
Kelly Hodis <668@holbertonschool.com>
2019-08-15 22:47:22 +0000

    Update print format

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     for state in objects:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
+            print("    {}: {}".format(city.id, city.name))
