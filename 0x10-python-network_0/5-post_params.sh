9c8b7837265f7a158d757f90a07b0b53a29b1894
Kelly Hodis <668@holbertonschool.com>
2019-08-20 15:09:04 -0400

    Edit line count, add shebang, add -s flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Takes in a URL, sends a POST request to URL, and displays response body.
-
-curl -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
