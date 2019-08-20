b8284c1428e127eb89aa26c5b1d510d8a44ffe98
Kelly Hodis <668@holbertonschool.com>
2019-08-20 16:14:18 -0400

    Add error handling

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,6 +7,14 @@ def find_peak(list_of_integers):
     '''Finds the peak in an unsorted list of integers.
     '''
     if list_of_integers:
+        if len(list_of_integers) < 3:
+            if len(list_of_integers) < 2:
+                return (list_of_integers[0])
+            else:
+                if list_of_integers[0] > list_of_integers[1]:
+                    return list_of_integers[0]
+                else:
+                    return list_of_integers[1]
         index = len(list_of_integers) // 2
         if list_of_integers[index - 1] <= list_of_integers[index]:
             if list_of_integers[index + 1] <= list_of_integers[index]:
