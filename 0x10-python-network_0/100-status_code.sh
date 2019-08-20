f2f9979712fda26edbfcb45500a3e35a4b680a52
Kelly Hodis <668@holbertonschool.com>
2019-08-20 17:26:38 -0400

    Update flags

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to URL passed and displays status code of response.
-curl -LIs -o /dev/null -w "%{http_code}" "$1"
+curl -s -o /dev/null -w "%{http_code}" -X POST "$1"
