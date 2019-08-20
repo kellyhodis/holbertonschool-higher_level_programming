9467cc280d08fedf8379a283949102a79e12d5a0
Kelly Hodis <668@holbertonschool.com>
2019-08-20 14:58:49 -0400

    Create 6-peak.py and 6-peak.txt

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Contains a function that finds a peak in a list of unsorted integers.
+"""
+
+
+def find_peak(list_of_integers):
+    '''Finds the peak in an unsorted list of integers.
+    '''
+    if list_of_integers:
+        index = len(list_of_integers) // 2
+        if list_of_integers[index - 1] <= list_of_integers[index]:
+            if list_of_integers[index + 1] <= list_of_integers[index]:
+                return list_of_integers[index]
+            else:
+                return find_peak(list_of_integers[index:])
+        else:
+            return find_peak(list_of_integers[:index + 1])
+    else:
+        return None
