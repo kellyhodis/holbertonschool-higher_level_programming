95433761a3e96c35abf1b4ee764de055d7974a7d
Kelly Hodis <668@holbertonschool.com>
2019-08-27 01:36:05 -0400

    Create 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This is a script that takes input from the user:
+- repository name
+- owner name
+and prints all commits by <sha>: <author name>
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
+                                                              sys.argv[1])
+    r = requests.get(url)
+    for i in r.json():
+        print("{}: {}".format(i['sha'], i['commit']['author']['name']))
