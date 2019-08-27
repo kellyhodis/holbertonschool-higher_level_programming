d7995b1f4ddf57bcd14e0a892c32e92905e42263
Kelly Hodis <668@holbertonschool.com>
2019-08-27 01:21:49 -0400

    Create 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This is a script that takes Github credentials from the user and
+uses the Github API to display the user id.
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/" + sys.argv[1]
+    r = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    print(r.json()['id'])
