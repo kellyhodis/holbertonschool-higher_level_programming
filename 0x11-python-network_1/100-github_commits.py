df23f5d75ec6434843ab0d1548fecb3b40f40240
Kelly Hodis <668@holbertonschool.com>
2019-08-27 01:39:54 -0400

    Update script

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
                                                               sys.argv[1])
     r = requests.get(url)
-    for i in r.json():
+    for i in r.json()[:10]:
         print("{}: {}".format(i.get('sha'),
                               i.get('commit').get('author').get('name')))
