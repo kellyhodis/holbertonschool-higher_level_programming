9da0dbb74347bf7db954f4b518c120d521de7fe7
Kelly Hodis <668@holbertonschool.com>
2019-08-26 21:07:53 -0400

    Create 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This is a script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays the body
+of the response decoded in utf-8.
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as x:
+        print(x.read().decode('utf-8'))
