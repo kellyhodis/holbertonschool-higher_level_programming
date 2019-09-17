d9768893c64ad38c3771da99c5c86d8307ae3d8f
Kelly Hodis <668@holbertonschool.com>
2019-09-17 16:00:21 -0400

    Update script

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-array.forEach(item => console.log(item));
+for (const statement of array) {
+  console.log(statement);
+}
