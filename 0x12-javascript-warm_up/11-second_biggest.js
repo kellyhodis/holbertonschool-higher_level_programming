8c9b741bacbf1c93ec613562085caee7cb9ec35f
Kelly Hodis <668@holbertonschool.com>
2019-09-18 01:53:23 -0400

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,11 @@ let max_ = Number.NEGATIVE_INFINITY;
 let secondMax = max_;
 if (process.argv.length === 2 | process.argv.length === 3) { console.log(0); } else {
   for (let i = 2; i < process.argv.length; i++) {
-    if (Math.floor(Number(process.argv[i])) > max_) {
+    if (parseInt(process.argv[i]) > max_) {
       secondMax = max_;
-      max_ = Math.floor(Number(process.argv[i]));
+      max_ = parseInt(process.argv[i]);
+    } else if (parseInt(process.argv[i]) === max_) {
+      secondMax = max_;
     }
   }
   console.log(secondMax);
